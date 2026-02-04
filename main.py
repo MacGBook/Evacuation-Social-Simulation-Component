8ea4a4d063258fa97b7bfdec9d2386655adb8f1b
Madeleine "Mac" Gagné (She/They) <115742976+MacGBook@users.noreply.github.com>
2026-02-03 20:57:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,26 +7,58 @@
 import csv
 
 # From evaluation_agent:
-from evaluation_agent import eval_response_dict
-from evaluation_agent import total_message_sum
+
+iterations = input("How many iterations of the social sim would you like to run? ")
 
 ##############################################################################################################################################################################################################################################################################################################################################################
 # COMPLETING THE RUN AND SAVING IT TO A CSV
 
+final_responses_dictionary = {}
+final_total_sum_dictionary = {}
+
+for number in range(int(iterations)):
+    iteration_name = "iteration_" + str(number)
+
+    from evaluation_agent import eval_response_dict
+
+    list_of_statements = eval_response_dict.values()
+
+
+    final_responses_dictionary[iteration_name] = list_of_statements
+
+    from evaluation_agent import total_message_sum
+    final_total_sum_dictionary[iteration_name] = total_message_sum
+    
+
+
+
+
 #Save individual rankings to their csv
 with open("ind_ag_output.csv", "w", newline="") as f:
-    writer = csv.DictWriter(f, eval_response_dict.keys())
+    writer = csv.DictWriter(f, final_responses_dictionary.keys())
     writer.writeheader()
-    writer.writerow(eval_response_dict)
+    writer.writerow(final_responses_dictionary)
 
-try:
-    with open("total_output.csv", mode='w', newline='') as file:
-        writer = csv.writer(file)
+
+
+
+#Save individual rankings to their csv
+#with open("total_output.csv.csv", "w", newline="") as f:
+ #   writer = csv.DictWriter(f, final_total_sum_dictionary.keys())
+  #  writer.writeheader()
+   # writer.writerow(final_total_sum_dictionary)
+
+
+
+
+#try:
+ #   with open("total_output.csv", mode='w', newline='') as file:
+  #      writer = csv.writer(file)
 
         # Write the integer as a single row
-        writer.writerow([total_message_sum])
-except IOError as e:
-    print(f"Error writing to file: {e}")
+   #     writer.writerow([total_message_sum])
+#except IOError as e:
+ #   print(f"Error writing to file: {e}")
 
 
  
