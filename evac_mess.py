18d53951c69966f968b0540030f6e6873c5be0fb
Gokul Prakash Ramesh <gokulprakashramesh@gmail.com>
2026-02-11 11:56:21 -0500

    Pushed runs of prompts 1 - 7 with the appropriate line indexes for each prompt in main.py

--- evac_mess.py
+++ evac_mess.py
@@ -1,2 +1,20 @@
 #Set the evacuation message to be used as a string:
-evacuation_message = "There's a bad hurricane coming. You need to evacuate!" 
+evacuation_message = "URGENT - FLASH FLOOD WARNING FROM THE WILMINGTON NATIONAL WEATHER SERVICE. The Emergency " \
+"Alert Systems have been activated. As of 1:24 PM on Sunday, September 16th, 2018, a " \
+"substantial flash flood warning is in effect for Manteo, North Carolina and the " \
+"surrounding areas." \
+"Beware - DANGEROUS FLOODWATERS ARE IMMINENT. Hurricane Florence has already brought up to " \
+"26 inches of rain and ANOTHER 3 to 6 inches are expected. Rain is falling at a rate of 1 " \
+"to 2 inches per hour, causing flash flooding." \
+"The counties near Wilmington, other major cities, and landmarks are at EXTREME RISK. The " \
+"escalating water levels pose a serious threat to life and property. Flash floods will " \
+"cause rivers and streams to overflow, roads to become submerged, and landslides in steeper" \
+"areas." \
+"Urgent actions needed - MOVE IMMEDIATELY to higher ground if you are in a flood-prone area" \
+"or if directed by authorities. DO NOT drive or walk into flooded areas. This is HIGHLY " \
+"RISKY and can be fatal. FOLLOW all instructions from your local authorities." \
+"Be prepared - SECURE your belongings and evacuate if necessary. Have a PLAN. Gather FOOD " \
+"and WATER for at least three days, a flashlight, batteries, meds, documents, and other " \
+"IMPORTANT ITEMS." \
+"This warning is set TO EXPIRE at 9:30 PM on Sunday, September 16th, 2018. Please stay " \
+"tuned for further updates and STAY SAFE."
--- main.py
+++ main.py
@@ -29,4 +29,11 @@ except IOError as e:
     print(f"Error writing to file: {e}")
 
 
- 
+# total_output.csv; ind_ag_output.csv; prompt number
+#  1 - 100; 1 - 200 : Prompt 1
+# 101 - 177; 201 - 354 : Prompt 2
+# 178 - 277; 355 - 554 : Prompt 3
+# 278 - 377; 555 - 754 : Prompt 4
+# 378 - 477; 755 - 954 : Prompt 5
+# 478 - 577; 955 - 1154 : Prompt 6
+# 578 - 677; 1155 - 1354 : Prompt 7
